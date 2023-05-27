281076e7e96b6325508a37672d57377e64090d5a
Hymn <danh208pt@gmail.com>
2023-05-27 10:00:02 +0700

    Commit

--- deploy_application/lint_check.sh
+++ deploy_application/lint_check.sh
@@ -1,10 +1,10 @@
 in=$1
 
-if [ in == "index.html" ];
+if [ $in == "index.html" ];
 then
-    echo "Lint Passed!!!"
-    exit 0;
+    echo "Lint Passed!!!";
+    exit 0
 else
-    echo "Lint Failed!!!"
-    exit 1;
+    echo "Lint Failed!!!";
+    exit 1
 fi
